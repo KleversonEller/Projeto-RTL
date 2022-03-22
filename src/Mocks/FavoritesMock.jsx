db4a2d52f06a661de38f9a388f74014bbd2d57ac
Kleverson Eller <kleverson.a16@gmail.com>
2022-03-22 18:48:52 -0300

    Requisito - 3

---
+++ src/Mocks/FavoritesMock.jsx
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: `This intelligent Pokémon roasts hard berries with
+    electricity to make them tender enough to eat.`,
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+    summary: `The flame on its tail shows the
+    strength of its life force. If it is weak, the flame also burns weakly.`,
+  },
+];
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react-dom';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../services/renderWithRouter';
+
+const pokemonsMock = require('../Mocks/FavoritesMock');
+
+describe('Teste a pagina de "Pokémons Favoritos"', () => {
+  it(`Verifica se é exibido na tela a mensagem 'No favorite pokemon found',
+  se a pessoa não tiver pokémons favoritos.`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    const title = screen.getByText(/No favorite pokemon found/i);
+    expect(title).toBeDefined();
+  });
+
+  it('Verifica se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemonsMock } />);
+    const pokemonsName = screen.getAllByTestId('pokemon-name');
+    expect(pokemonsName).toBeDefined();
+    expect(pokemonsName.length).toBe(2);
+    expect(pokemonsName[0]).toHaveTextContent('Pikachu');
+    expect(pokemonsName[1]).toHaveTextContent('Charmander');
+  });
+});
